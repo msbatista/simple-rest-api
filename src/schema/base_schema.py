c992be2e0165b1e662283e243ce1c9364acbc8fd
Marcelo Silva Batista <silvabatistamarcelo@gmail.com>
2019-10-27 23:27:26 -0300

    Make id field required

--- src/schema/base_schema.py
+++ src/schema/base_schema.py
@@ -2,7 +2,7 @@ from marshmallow import fields, Schema
 
 
 class BaseSchema(Schema):
-    id = fields.Integer()
+    id = fields.Integer(required=True)
     type = fields.Boolean(required=True)
     timestamp = fields.DateTime(required=True)
     call_id = fields.Integer(required=True)
